3e67f78ba5cdca3e4a04fe96fb6436a6e024c006
Endri Simakou <e.simakou@donboscosandona.it>
2023-03-30 13:19:22 +0200

    Da sistemare Footer

--- dev/albaniandevils/Footer.js
+++ dev/albaniandevils/Footer.js
@@ -1,5 +1,121 @@
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
+
+import Carousel from '/components/Carousel'
+
+
+let slides = [
+    {
+        titolo: "..",
+        descrizione: "",
+        colore: "",
+        immagine
+    }
+]
 export default function Footer(props) {
     return (
-        <h1>Il mio Footer</h1>
+        <Container
+            maxWidth={true}
+            disableGutters={true}
+        >
+            <Box
+                sx={{
+                    position: 'relative',
+                    top: 0,
+                    bottom: 0,
+                    right: 0,
+                    left: 0,
+                    backgroundColor: props.color,
+                }}
+            >
+                <Container maxWidth="lg" sx={{ marginTop: '10px', marginBottom: '35px' }}>
+                    <Grid container spacing={3}>
+                        { /* Chi siamo */}
+                        <Grid item xs={12} lg={4}>
+                            <Typography component="h1" color="White" paddingBottom="2rem">
+                                {props.title1}
+                            </Typography>
+                            <Typography component="h5" color="darkgrey" paragraph>
+                                {props.description1}
+                            </Typography>
+                        </Grid>
+
+                        { /* Social Feed */}
+                        <Grid item xs={12} lg={4}>
+                            <Typography component="h1" color="White" paddingBottom="2rem">
+                                {props.title2}
+                            </Typography>
+                            <Typography component="h1" color="White" >
+                                {
+                                    props.socials.map((social) => (
+                                        <Container sx={{ padding: "0.5rem" }}>
+                                            <Link
+                                                color="inherit"
+                                                noWrap
+                                                key={social.title}
+
+                                                variant="body2"
+                                                href={social.url}
+                                                sx={{ p: 1, flexShrink: 0, }}
+                                            >
+                                                <img src={social.imageUrl} width="20" />
+                                                {social.title}
+                                            </Link>
+                                        </Container>
+                                    ))
+                                }
+                            </Typography>
+                        </Grid>
+
+                        { /* Immagini */}
+                        <Grid item xs={12} lg={4}>
+                            <Typography component="h1" color="White" paddingBottom="2rem">
+                                {props.title3}
+                            </Typography>
+                            <Grid container spacing={2} minHeight={160}>
+                                {
+                                    props.images.map((image) => (
+                                        <Grid item xs={4} display="flex" justifyContent="center" alignItems="center">
+                                                <Carousel
+                                                    slides={slides}
+                                                    maxWidth={false}
+                                                    height="90vh"
+                                                ></Carousel>
+                                                <Link
+                                                    color="inherit"
+                                                    noWrap
+                                                    variant="body2"
+                                                    sx={{ p: 1, flexShrink: 3, }}
+                                                >
+                                                    <img src={image.images} width="120" />
+                                                </Link>
+                                        </Grid>
+                                    ))
+                                }
+                            </Grid>
+                        </Grid>
+
+                        { /* Linea */}
+                        <Grid item xs={12}>
+                            <Typography component="h1">
+                                <hr />
+                            </Typography>
+                        </Grid>
+
+                        { /* Copyright */}
+                        <Grid item xs={12} lg={12}>
+                            <Typography component="h1" color="White" sx={{ marginLeft: '420px', marginBottom: '35px' }}>
+                                {props.copyright}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Box>
+        </Container>
     )
 }
--- pages/dev/albaniandevils/index.js
+++ pages/dev/albaniandevils/index.js
@@ -1,15 +1,39 @@
 import Layout from 'components/Layout'
 import Footer from '/dev/albaniandevils/Footer'
 
+
+
+let socials = [
+    { title: 'Facebook', imageUrl: 'https://www.donboscosandona.it/img/ck/1cffc9d197e15de1f72a89477cc75e56073980b0.png', url: 'https://www.facebook.com/donboscosandona/?fref=ts' },
+    { title: 'Instagram', imageUrl: 'https://www.donboscosandona.it/img/ck/5c1c2a74750c62b3349df0d555ea9a26d1c6e8af.png', url: 'https://www.instagram.com/donboscosandona/' },
+    { title: 'Youtube', imageUrl: 'https://www.donboscosandona.it/img/ck/36189d32ee9d4be0a612c292a430106948c85bfc.png', url: 'https://www.youtube.com/channel/UCZ2sxe9w7Yf9lP4nl65oAvg' },
+    { title: 'Linkedin', imageUrl: 'https://www.donboscosandona.it/img/ck/55ed185e94be2aa2a1d854487273a4d139e5475f.png', url: 'https://www.linkedin.com/company/c-f-p-don-bosco/' },
+]
+
+let images = [
+    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
+    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
+    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
+    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
+    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
+    { title: '', imageUrl: 'https://www.donboscosandona.it/img/column/f65e4622afa225a8f6f407cff0b9e616279eea44.jpg?1570719707', },
+]
+
+
 export default function Page() {
     return (
-        <Layout>
-            <h1>Ciao</h1>
-            <Footer />
-        </Layout>
-
-
+        <Footer
+            imageUrl="https://donboscoitalia.it/wp-content/uploads/2019/07/Copia-di-donboscoitalia-foto-11.jpg"
+            color="#722F37"
+            opacity={1}
+            title1="About Us"
+            description1="Creative Tim is a startup..."
+            title2="Social Feed"
+            socials={socials}
+            title3="I Settori"
+            images={images}
+            //menu={sezioni}
+            copyright="Copyright (C) 2023 9dreams Agency."
+        />
     )
-
-
 }
