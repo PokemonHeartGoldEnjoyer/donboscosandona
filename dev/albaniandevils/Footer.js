dae75d4856ef8e28db5ee3e89760be31f0552ef9
Endri Simakou <e.simakou@donboscosandona.it>
2023-03-30 11:54:18 +0200

    Creato il componente Footer vuoto

---
+++ dev/albaniandevils/Footer.js
@@ -0,0 +1,5 @@
+export default function Footer(props) {
+    return (
+        <h1>Il mio Footer</h1>
+    )
+}
---
+++ pages/dev/albaniandevils/index.js
@@ -0,0 +1,15 @@
+import Layout from 'components/Layout'
+import Footer from '/dev/albaniandevils/Footer'
+
+export default function Page() {
+    return (
+        <Layout>
+            <h1>Ciao</h1>
+            <Footer />
+        </Layout>
+
+
+    )
+
+
+}
